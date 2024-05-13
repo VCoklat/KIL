ae366c94b644d027d9aae1737d09e43941c1830f
VCoklat <VCoklat@users.noreply.github.com>
2024-05-13 04:43:42 +0000

    Update run_routenet.sh to use conda environment for Python execution

--- run_routenet.sh
+++ run_routenet.sh
@@ -7,7 +7,7 @@ fi
 
 if [[ "$1" = "train" ]]; then
 
-    /mnt/c/Users/yohan/.conda/envs/py36/python.exe routenet_with_link_cap.py train --hparams="l2=0.1,dropout_rate=0.5,link_state_dim=32,path_state_dim=32,readout_units=256,learning_rate=0.001,T=8"  --train  $PATH_TO_DATASET/$2/tfrecords/train/*.tfrecords --train_steps $3 --eval_ $PATH_TO_DATASET/$2/tfrecords/evaluate/*.tfrecords --model_dir ./CheckPoints/$2
+    /opt/conda/envs/ProtoShotXAI/bin/python routenet_with_link_cap.py train --hparams="l2=0.1,dropout_rate=0.5,link_state_dim=32,path_state_dim=32,readout_units=256,learning_rate=0.001,T=8"  --train  /workspaces/KIL/datasets/$2/tfrecords/train/*.tfrecords --train_steps $3 --eval_ /workspaces/KIL/datasets/$2/tfrecords/evaluate/*.tfrecords --model_dir ./CheckPoints/$2
 
 fi
 
